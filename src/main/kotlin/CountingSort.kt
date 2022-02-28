d408891283b7a1b5c03a73ee26db57349699045d
sloning <i.96petrov@gmail.com>
2022-02-28 18:55:59 +0300

    my battery is dying

--- src/main/kotlin/CountingSort.kt
+++ src/main/kotlin/CountingSort.kt
@@ -2,19 +2,19 @@ fun countingSort(arr: List<Int>): List<Int> {
     val max: Int = arr.maxOrNull()!!
     val min: Int = arr.minOrNull()!!
     val range = max - min + 1
-    val count = IntArray(range)
+    val count = HashMap<Int, Int>()
     val output = IntArray(arr.size)
     for (element in arr) {
-        count[element - min]++
+        count[element] = 1
     }
 
-    for (i in 1 until count.size) {
-        count[i] += count[i - 1]
+    for (i in 2 until count.size) {
+        count[i] = count[i - 1]?.let { count[i]?.plus(it) }!!
     }
 
     for (i in arr.size - 1 downTo 0) {
-        output[count[arr[i] - min] - 1] = arr[i]
-        count[arr[i] - min]--
+        output[count[arr[i] - min]?.minus(1)!!] = arr[i]
+        count[arr[i] - min]?.minus(1)
     }
     return output.toList()
 }
